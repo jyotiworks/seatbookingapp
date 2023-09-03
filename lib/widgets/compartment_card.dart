51cd07c0680e1e39a2dd814448b82a05529194b3
motocapt <jyotiworks0428@gmail.com>
2023-09-03 17:59:44 +0530

    seat App

---
+++ lib/widgets/compartment_card.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:seat_booking_app/constants.dart';
+import 'package:seat_booking_app/widgets/seat_widget.dart';
+
+class CompartmentCard extends StatelessWidget {
+  final int compartmentNum;
+  final int chosenSeatNumber;
+  final Tier tier;
+  const CompartmentCard({
+    Key? key,
+    required this.compartmentNum,
+    required this.chosenSeatNumber,
+    required this.tier,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    int firstSeatNum = (tier == Tier.tier3)
+        ? ((compartmentNum - 1) * 8 + 1)
+        : ((compartmentNum - 1) * 6 + 1);
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: kCompartmentColour,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: (tier == Tier.tier3)
+            ? Column(
+          children: [
+            Row(
+              children: [
+                Seat(
+                  seatNumber: firstSeatNum,
+                  isLeft: true,
+                  chosenSeatNumber: chosenSeatNumber,
+                  tier: Tier.tier3,
+                ),
+                Seat(
+                  seatNumber: firstSeatNum + 1,
+                  isLeft: true,
+                  chosenSeatNumber: chosenSeatNumber,
+                  tier: Tier.tier3,
+                ),
+                Seat(
+                  seatNumber: firstSeatNum + 2,
+                  isLeft: true,
+                  chosenSeatNumber: chosenSeatNumber,
+                  tier: Tier.tier3,
+                ),
+                const Spacer(),
+                Seat(
+                  seatNumber: firstSeatNum + 6,
+                  isLeft: true,
+                  chosenSeatNumber: chosenSeatNumber,
+                  tier: Tier.tier3,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Row(
+              children: [
+                Seat(
+                  seatNumber: firstSeatNum + 3,
+                  isLeft: false,
+                  chosenSeatNumber: chosenSeatNumber,
+                  tier: Tier.tier3,
+                ),
+                Seat(
+                  seatNumber: firstSeatNum + 4,
+                  isLeft: false,
+                  chosenSeatNumber: chosenSeatNumber,
+                  tier: Tier.tier3,
+                ),
+                Seat(
+                  seatNumber: firstSeatNum + 5,
+                  isLeft: false,
+                  chosenSeatNumber: chosenSeatNumber,
+                  tier: Tier.tier3,
+                ),
+                const Spacer(),
+                Seat(
+                  seatNumber: firstSeatNum + 7,
+                  isLeft: false,
+                  chosenSeatNumber: chosenSeatNumber,
+                  tier: Tier.tier3,
+                ),
+              ],
+            ),
+          ],
+        )
+            : Column(
+          children: [
+            Row(
+              children: [
+                Seat(
+                  seatNumber: firstSeatNum,
+                  isLeft: true,
+                  chosenSeatNumber: chosenSeatNumber,
+                  tier: Tier.tier2,
+                ),
+                Seat(
+                  seatNumber: firstSeatNum + 1,
+                  isLeft: true,
+                  chosenSeatNumber: chosenSeatNumber,
+                  tier: Tier.tier2,
+                ),
+                const Spacer(),
+                Seat(
+                  seatNumber: firstSeatNum + 4,
+                  isLeft: true,
+                  chosenSeatNumber: chosenSeatNumber,
+                  tier: Tier.tier2,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Row(
+              children: [
+                Seat(
+                  seatNumber: firstSeatNum + 2,
+                  isLeft: false,
+                  chosenSeatNumber: chosenSeatNumber,
+                  tier: Tier.tier2,
+                ),
+                Seat(
+                  seatNumber: firstSeatNum + 3,
+                  isLeft: false,
+                  chosenSeatNumber: chosenSeatNumber,
+                  tier: Tier.tier2,
+                ),
+                const Spacer(),
+                Seat(
+                  seatNumber: firstSeatNum + 5,
+                  isLeft: false,
+                  chosenSeatNumber: chosenSeatNumber,
+                  tier: Tier.tier2,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/custom_alert_box.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+
+class CustomAlertBox extends StatelessWidget {
+  final Tier tier;
+  const CustomAlertBox({super.key, required this.tier});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints(maxHeight: 80),
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(),
+      child: Text(
+        (tier == Tier.tier3)
+            ? "Please choose a valid seat number between 1 and 32"
+            : 'Please choose a valid seat number between 1 and 24',
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
